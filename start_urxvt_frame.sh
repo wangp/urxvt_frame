29524e4fd52cbc47a8cefb5a08f86ed009187f12
Peter Wang <novalazy@gmail.com>
2012-07-28 15:45:01 +1000

    Convert start_urxvt_frame to sh script.

--- start_urxvt_frame.sh
+++ start_urxvt_frame.sh
@@ -1,18 +1,23 @@
-#!/bin/zsh
+#!/bin/sh -e
+# 
+# This script starts the urxvt daemon (urxvtd) if not already running,
+# before running urxvt_frame.  You will need to configure urxvt_frame
+# to run 'urxvtc' instead of 'urxvt' in urxvt_frame.conf.
 
-if ! pgrep -u $UID urxvtd >/dev/null
+# Check if urxvtd running for the current user.
+if ! pgrep -u "$UID" urxvtd >/dev/null
 then
     urxvtd -q -o -f
 fi
 
-PROG=${0:h}/urxvt_frame
+PROG=$(dirname $0)/urxvt_frame
 
-if (( $# == 0 ))
+if test $# -eq 0
 then
-    exec $PROG
-elif [[ $1 == "-e" ]]
+    exec "$PROG"
+elif test "$1" = "-e"
 then
-    exec $PROG "$@"
+    exec "$PROG" "$@"
 else
-    exec $PROG -e "$@"
+    exec "$PROG" -e "$@"
 fi
